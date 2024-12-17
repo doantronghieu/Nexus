abee90ce315c355682ad7d5aca9113718e7dcd9f
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-12-17 11:33:31 +0700

    [UI] Car

--- ui/projects/car/lib/core/constants/control_icons.dart
+++ ui/projects/car/lib/core/constants/control_icons.dart
@@ -1,20 +1,28 @@
 class ControlIcons {
   static const String engineControl = '''
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z" fill="currentColor"/>
-      <path d="M12 7V13L16 15.5L15.2 16.8L10 13.5V7H12Z" fill="currentColor"/>
+      <!-- Common engine/start button symbol: Power symbol with ring -->
+      <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2" fill="none"/>
+      <path d="M12 6v12" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+      <path d="M12 6a6 6 0 0 1 0 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
     </svg>
   ''';
 
   static const String doorLock = '''
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M18 8H17V6C17 3.24 14.76 1 12 1C9.24 1 7 3.24 7 6V8H6C4.9 8 4 8.9 4 10V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V10C20 8.9 19.1 8 18 8ZM9 6C9 4.34 10.34 3 12 3C13.66 3 15 4.34 15 6V8H9V6ZM18 20H6V10H18V20ZM12 17C13.1 17 14 16.1 14 15C14 13.9 13.1 13 12 13C10.9 13 10 13.9 10 15C10 16.1 10.9 17 12 17Z" fill="currentColor"/>
+      <!-- Car with lock symbol -->
+      <path d="M17 12H7c-.55 0-1 .45-1 1v2c0 .55.45 1 1 1h10c.55 0 1-.45 1-1v-2c0-.55-.45-1-1-1zm-1 2H8v-1h8v1z" fill="currentColor"/>
+      <path d="M18 10h-1V7c0-2.21-1.79-4-4-4h-2C8.79 3 7 4.79 7 7v3H6c-1.1 0-2 .9-2 2v7c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-7c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM9 7c0-1.1.9-2 2-2h2c1.1 0 2 .9 2 2v3H9V7z" fill="currentColor"/>
     </svg>
   ''';
 
   static const String climateControl = '''
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM15.5 11C16.33 11 17 10.33 17 9.5C17 8.67 16.33 8 15.5 8C14.67 8 14 8.67 14 9.5C14 10.33 14.67 11 15.5 11ZM8.5 11C9.33 11 10 10.33 10 9.5C10 8.67 9.33 8 8.5 8C7.67 8 7 8.67 7 9.5C7 10.33 7.67 11 8.5 11ZM12 17.5C14.33 17.5 16.31 16.04 17.11 14H6.89C7.69 16.04 9.67 17.5 12 17.5Z" fill="currentColor"/>
+      <!-- AC/Fan symbol commonly used in cars -->
+      <circle cx="12" cy="12" r="3" fill="currentColor"/>
+      <path d="M12 5v2M12 17v2M5 12h2M17 12h2" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+      <path d="M7.05 7.05l1.41 1.41M15.54 15.54l1.41 1.41M7.05 16.95l1.41-1.41M15.54 8.46l1.41-1.41" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z" fill="currentColor"/>
     </svg>
   ''';
 }
